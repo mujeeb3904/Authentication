b8aaa8144c5ee3fe0a66c1cb7ba05eb1f8559d77
Mujeeb Khan <mujeebblouch7036@gmail.com>
2024-11-02 10:29:25 +0500

    Node Mailer

---
+++ nodeMailer/nodeMailer.js
@@ -0,0 +1,20 @@
+
+// const sendVerificationEmail = (to, subject, text, res) => {
+//     const mailOptions = {
+//         from: process.env.EMAIL,
+//         to: "mujeebdevoff@gmail.com",
+//         subject: "Verification Code",
+//         text:  "Your verification code",
+//     };
+
+   
+// transporter.sendMail(mailOptions, function(error, info){
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// };
+
+// module.exports = sendVerificationEmail;
